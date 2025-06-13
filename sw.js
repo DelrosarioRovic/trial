048478764f3d216d66784d5afc07474a7a1a540e
DelrosarioRovic <rovicdelrosario01@gmail.com>
2025-06-13 09:21:44 +0800

    configure pwa

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'legends-todo-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/css/login-carousel.css',
-  '/css/login.css',
-  '/js/auth-transition.js',
-  '/js/login-carousel.js',
-  '/js/prevent-zoom.js',
-  '/assets/images/login-carousel-1.jpg',
-  '/assets/images/login-carousel-2.jpg',
-  '/assets/images/login-carousel-3.jpg',
-  '/assets/icons/google.png',
-  '/assets/icons/facebook.png',
-  '/assets/icons/twitter-alt.png'
+  './',
+  './index.html',
+  './css/style.css',
+  './css/login-carousel.css',
+  './css/login.css',
+  './js/auth-transition.js',
+  './js/login-carousel.js',
+  './js/prevent-zoom.js',
+  './assets/images/login-carousel-1.jpg',
+  './assets/images/login-carousel-2.jpg',
+  './assets/images/login-carousel-3.jpg',
+  './assets/icons/google.png',
+  './assets/icons/facebook.png',
+  './assets/icons/twitter-alt.png'
 ];
 
 self.addEventListener('install', (e) => {
